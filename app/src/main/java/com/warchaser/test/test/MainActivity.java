b447584364b338037e2bb9e6c9f6c5df75fe9477
wu.shb <wu.shb@neusoft.com>
2016-06-21 18:21:31 +0800

    fix # init this project Signed-off-by: wu.shb <wu.shb@neusoft.com>

---
+++ app/src/main/java/com/warchaser/test/test/MainActivity.java
@@ -0,0 +1,29 @@
+package com.warchaser.test.test;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.warchaser.test.util.VerticalTextView;
+
+public class MainActivity extends Activity
+{
+    private VerticalTextView mTextView1;
+
+    private VerticalTextView mTextView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        mTextView1 = (VerticalTextView) findViewById(R.id.text1);
+        mTextView2 = (VerticalTextView) findViewById(R.id.text2);
+//
+        mTextView1.setText(getResources().getString(R.string.test_string2));
+        mTextView2.setText(getResources().getString(R.string.test_string1));
+
+    }
+}
